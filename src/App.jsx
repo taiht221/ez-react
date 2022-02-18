0bb3653dd33e92540e4cf7d9e544e4454908787c
Tai Dentsu <tai.huynh1@iprospect.com>
2022-02-18 10:39:55 +0700

    set up new react project with vitejs

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import caterotyApi from './api/categoryApi'
+
+function App() {
+  const [count, setCount] = useState(0)
+  useEffect(async () => {
+    try {
+      const pro = await caterotyApi.getAll()
+      console.log(pro)
+    } catch (error) {}
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const Test: React.FunctionComponent<{}> = (props) => {
+  return (
+    <div>
+      <Link to="/">App</Link>
+    </div>
+  )
+}
+
+export default Test
